89c1c9209d0d5a268b0697f44d4ee032a8c67af4
Noey Ignacio <francis.ignacio@aelluminate.com>
2024-11-02 04:39:21 +0800

    refactor: simplify table types and add ColorBoxProps interface

--- types/table.ts
+++ types/table.ts
@@ -1,30 +1,3 @@
-import { ReactNode } from "react"
-import { CellContext } from "@tanstack/react-table"
-
-export interface DataTableProps<T extends object> {
-  data: T[]
-  headers: Array<keyof T | HeaderConfig<T>>
-}
-
-export interface CollectionTableItem {
-  id: number
-  name: string
-
-  description: string
-  color: string
-}
-
-export interface HeaderConfig<T> {
-  name: keyof T
-  class?: string
-  header?: string
-  cell?: (context: CellContext<T, unknown>) => ReactNode
-}
-
-export interface ColumnMeta {
-  className?: string
-}
-
-export interface ContentProps {
-  color: string
+export interface TableCellProps extends React.TdHTMLAttributes<HTMLTableCellElement> {
+  isWrap?: boolean
 }
